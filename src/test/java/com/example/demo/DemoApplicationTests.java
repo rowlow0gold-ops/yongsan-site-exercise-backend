f0ba115bce839b6345657c3ab21e5f664df135bc
장민호 <rowlow302606@gmail.com>
2026-04-02 17:50:48 +0900

    ci: add test job to pipeline

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,24 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "app.jwt.secret=testsecrettestsecrettestsecrettestsecret",
+        "app.jwt.accessTtlSeconds=900",
+        "app.jwt.refreshTtlSeconds=1209600",
+        "app.jwt.refreshCookieName=refresh_token",
+        "app.oauth2.redirect-uri=http://localhost:5173",
+        "spring.security.oauth2.client.registration.google.client-id=test-client-id",
+        "spring.security.oauth2.client.registration.google.client-secret=test-client-secret",
+        "spring.security.oauth2.client.registration.google.scope=email,profile"
+})
 class DemoApplicationTests {
 
     @Test
